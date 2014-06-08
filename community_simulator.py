a796c14131fff43e623eaade0762a322132f4f8f
EricaJohnson <edj@uoregon.edu>
2014-06-08 14:48:08 -0700

    added time funciton in community_simulator

--- community_simulator.py
+++ community_simulator.py
@@ -7,14 +7,21 @@ Created 25 May  2014
 Edited  08 June 2014
 '''
 
-from person_class import person
+from person_class import Person
 
-def main(person):
+def main():
     person_list = list()
     for i in range(15):
-        person_list.append(person())
+        person_list.append(Person())
     for person in person_list:
         print(person.name)
+        
+def time():
+    for person in person_list:
+        if Person.death_chance() == True:
+            person_list.remove(person)
+        person.age += 1
+
 
 main(person)
 
